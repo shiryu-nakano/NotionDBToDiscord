475ba6685248aacc00fec2f6e8ccf0f37abaa07c
shiryu-nakano <shiryu.nakano03@gmail.com>
2026-07-08 23:18:45 +0900

    [mod] when target level is set multiply, the algorithm pick one problem from different levels

--- run/atcoder_discord.py
+++ run/atcoder_discord.py
@@ -14,9 +14,58 @@ if __name__ == "__main__":
     '''
 
     settings, target = get_atcoder_target()
+    print("======= Basic Configuration =======")
+    print(f"[INFO] Script path: {__file__}")
+    print(f"[INFO] AtCoder user ID: {settings.ATCODER_USER_ID}")
+    print(f"[INFO] AtCoder levels: {settings.ATCODER_LEVELS}")
+    print(f"[INFO] AtCoder exclude prefixes: {settings.ATCODER_EXCLUDE_PREFIXES}")
+    print(f"[INFO] Discord webhook URL: {settings.DISCORD_WEBHOOK_URL_ATCODER}")
+    print(f"[INFO] Message greeting: {settings.MESSAGE_GREETING_ATCODER}")
+    print(f"[INFO] Message template: {settings.MESSAGE_TEMPLATE}")
+    
+    if len(settings.ATCODER_LEVELS) == 2 :
+        print("============== Process ===============")
+        from pick.atcoder import pick_unsolved_problem
+        from utils.config import get_atcoder_target
+        from utils.message import build_daily_message
+        
+        results = target["get"]()
+        print(f"[INFO] Retrieved {len(results[0])} problems, {len(results[1])} AC IDs")
+        picks: list[tuple[str, str]] = [
+            pick_unsolved_problem(results, [level], settings.ATCODER_EXCLUDE_PREFIXES)
+            for level in settings.ATCODER_LEVELS
+            ]
+        print(f"[INFO] Picked problems: {picks}")
 
-    results = target["get"]()
-    title, url = target["pick"](results)
-    message = build_daily_message(title, url, settings, target["greeting"])
-    print(f"[INFO] Generated message:\n{message}")
-    target["send"](message, target["webhook"])
+        lines = []
+        progress:str = f"今の所あなたは{len(results[1])}問をAC済みです！" if len(results[1]) > 0 else "まだ問題がありません。"
+        for title, url in picks:
+            lines.append(f"{title}\n{url}")
+        lines.append(progress)
+        combined_body = "\n\n".join(lines)
+        message = f"\nこんにちは\n{target['greeting']}\n{combined_body}"
+        target["send"](message, target["webhook"])
+        print("========== Message ===========")
+        print(f"{message}")
+        print("==============================")
+        
+    else:
+        results = target["get"]()
+        print(f"[INFO] Retrieved {len(results[0])} problems, {len(results[1])} AC IDs")
+
+        title, url = target["pick"](results)
+        print(f"[INFO] Picked problem: {title} ({url})")
+
+        print("======= Message =======")
+        message = build_daily_message(title, url, settings, target["greeting"])
+        print(f"{message}")
+        #target["send"](message, target["webhook"])
+        print("=======================")
+
+    print("[INFO][DONE] AtCoder→Discord process completed.")
+
+    
+
+
+
+
